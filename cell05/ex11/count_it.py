223d033550261ad7427916d2bdb19b84aa2ca7f6
Rinlanee25 <67050487@kmitl.ac.th>
2026-09-23 17:38:17 +0700

    add ex11 cell05

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) <= 1:
+    print("none")
+else:
+    params = sys.argv[1:]
+    print(f"parameters: {len(params)}")
+    
+    for param in params:
+        print(f"{param}: {len(param)}")
